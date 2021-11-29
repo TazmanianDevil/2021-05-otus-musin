608f8e75abe96a72cc17702f1dd7fd3478368477
TazmanianDevil <musinmh@gmail.com>
2021-11-29 23:23:32 +0300

    Практическая работа
    ДОбавлены сервисы автора, жанра и книги

--- Online-Library/online-library-genre-service/src/main/java/ru/otus/GenreServiceApplication.java
+++ Online-Library/online-library-genre-service/src/main/java/ru/otus/GenreServiceApplication.java
@@ -3,14 +3,13 @@ package ru.otus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.http.ResponseEntity;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class LibraryService {
+public class GenreServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LibraryService.class, args);
+        SpringApplication.run(GenreServiceApplication.class, args);
     }
 
 }
